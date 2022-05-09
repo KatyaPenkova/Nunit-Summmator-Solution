53cc0558190a312673cdb055d24addaa795cc861
KatyaPenkova <104911279+KatyaPenkova@users.noreply.github.com>
2022-05-09 23:26:03 +0300

    Initial commit

---
+++ Summator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Summator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            long result = Summator.Sum(new int[] { 1, 2, 3, 4, 5, 6 });
+            if (result == 21)
+            {
+                Console.WriteLine("TEST PASS");
+            }
+            else
+            {
+                Console.WriteLine("TEST FAIL");
+            }
+            long Average = Summator.Average(new int[] { 5, 10, 15 });
+            if (Average == 10)
+            {
+                Console.WriteLine("TEST PASS");
+            }
+            else
+            {
+                Console.WriteLine("TEST FAIL");
+            }
+        }
+    }
+}
